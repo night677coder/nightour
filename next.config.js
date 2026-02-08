4b808b917c1e2302b3168301fcb38b48d3da39fc
night677coder <banuchander277@gmail.com>
2026-02-08 15:05:45 +0530

    Remove static export for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     domains: ['image.tmdb.org', 's4.anilist.co'],
     unoptimized: true,
@@ -8,9 +7,6 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_TMDB_API_KEY: process.env.NEXT_PUBLIC_TMDB_API_KEY,
   },
-  basePath: '/nightour',
-  assetPrefix: '/nightour/',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
